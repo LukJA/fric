868fedeb3bf40537e2776de4dda3c2c3b591dfa4
Luke Andrews <l.j.andrews.uk@gmail.com>
2023-06-05 20:30:00 +0100

    Basic SV build system

---
+++ hdl/dff.sv
@@ -0,0 +1,20 @@
+module signed_add (clk, a, b, q);
+	parameter WIDTH=8;
+	input logic clk;
+	input logic signed [WIDTH-1:0] a, b;
+	output logic signed [WIDTH-1:0] q;	
+
+// assign q = a + b;
+
+always_ff @(posedge clk) begin
+	q <= a + b;
+end
+
+// `ifdef COCOTB_SIM
+// initial begin
+//   $dumpfile ("trace.vcd");
+//   $dumpvars (0, signed_add);
+// end
+// `endif
+endmodule : signed_add
+
