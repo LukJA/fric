5e4935a9149aef494acf2b94edec5e3f73ebd2bb
Luke Andrews <l.j.andrews.uk@gmail.com>
2023-06-25 17:35:29 +0100

    Assign adder first pipe stage

--- hdl/sv/posit_adder.sv
+++ hdl/sv/posit_adder.sv
@@ -4,6 +4,7 @@ module posit_adder #(
 	parameter WIDTH=7,
     parameter EN=1)(
 	input logic clk,
+	input logic rst,
 	input logic [WIDTH-1:0] a, b,
 	output logic [WIDTH-1:0] q
     );
@@ -15,17 +16,47 @@ logic signed [7:0] b_regime, b_exponent;
 logic unsigned [7:0] b_mantissa; 
 
 
-format_decoder a_decode (.posit(a),
+format_decoder  #(WIDTH,EN) a_decode (.posit(a),
 						.sign(a_sign),
 						.regime(a_regime),
 						.exponent(a_exponent),
 						.mantissa(a_mantissa));
 
-format_decoder b_decode (.posit(b),
+format_decoder  #(WIDTH,EN) b_decode (.posit(b),
 						.sign(b_sign),
 						.regime(b_regime),
 						.exponent(b_exponent),
 						.mantissa(b_mantissa));
+
+// first pipeline register
+
+logic dc_a_sign, dc_b_sign;
+logic signed [7:0] dc_a_regime, dc_a_exponent;
+logic unsigned [7:0] dc_a_mantissa;
+logic signed [7:0] dc_b_regime, dc_b_exponent; 
+logic unsigned [7:0] dc_b_mantissa; 
+
+always_ff @(posedge clk) begin
+	if (~rst) begin
+		dc_a_sign <= 'b0;
+		dc_a_regime <= 'b0;
+		dc_a_exponent <= 'b0;
+		dc_a_mantissa <= 'b0;
+		dc_b_sign <= 'b0;
+		dc_b_regime <= 'b0;
+		dc_b_exponent <= 'b0;
+		dc_b_mantissa <= 'b0;
+	end else begin	
+		dc_a_sign <= a_sign;
+		dc_a_regime <= a_regime;
+		dc_a_exponent <= a_exponent;
+		dc_a_mantissa <= a_mantissa;
+		dc_b_sign <= b_sign;
+		dc_b_regime <= b_regime;
+		dc_b_exponent <= b_exponent;
+		dc_b_mantissa <= b_mantissa;
+	end
+end
 
 assign q = a + b;
 
--- hdl/test_posit_adder.py
+++ hdl/test_posit_adder.py
@@ -3,47 +3,48 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, ReadOnly
 from cocotb.types import LogicArray
 from cocotb.clock import Timer
+from cocotb.binary import BinaryValue
 
 
 @cocotb.test()
 async def test_posit_adder_top(dut):
 
     dut._log.warning(f"Test {__name__} Starting...")
-    # set a, b val
+    dut._log.info(f"1 + 2 = 3")
+
     dut.a.value = 0
     dut.b.value = 0
+    dut.rst.value = 0
 
     # 10ns system clock, start it low
     clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start(start_high=False))
+    await RisingEdge(dut.clk)
+    await FallingEdge(dut.clk)
+    dut.rst.value = 1
+
+    one = BinaryValue("0100000") ## 1
+    dut.a.value = one
+    two = BinaryValue("0101000") ## 2
+    dut.b.value = two
 
     await RisingEdge(dut.clk)
-    await ReadOnly() # wait for signals
-    assert dut.q.value == 0
+    await FallingEdge(dut.clk)
+
+    # dut._log.info(f"Output: {dut.regime_len.value}")
+    dut._log.info(f"a   : {dut.a.value}")
+    dut._log.info(f"a_s : {dut.dc_a_sign.value}")
+    dut._log.info(f"a_r : {dut.dc_a_regime.value}")
+    dut._log.info(f"a_e : {dut.dc_a_exponent.value}")
+    dut._log.info(f"a_m : {dut.dc_a_mantissa.value}")
+
+    dut._log.info(f"b   : {dut.b.value}")
+    dut._log.info(f"b_s : {dut.dc_b_sign.value}")
+    dut._log.info(f"b_r : {dut.dc_b_regime.value}")
+    dut._log.info(f"b_e : {dut.dc_b_exponent.value}")
+    dut._log.info(f"b_m : {dut.dc_b_mantissa.value}")
 
     await FallingEdge(dut.clk)
-    dut.a.value = 1
-    dut.b.value = 2
-
-    await RisingEdge(dut.clk)
-    await ReadOnly() # wait for signals
-    assert dut.q.value == 3
-
-    await FallingEdge(dut.clk)
-    dut.a.value = 3
-    dut.b.value = 4
-
-    await RisingEdge(dut.clk)
-    await ReadOnly() # wait for signals
-    assert dut.q.value == 7
-
-    await FallingEdge(dut.clk)
-    dut.a.value = 5
-    dut.b.value = 6
-
-    await RisingEdge(dut.clk)
-    await ReadOnly() # wait for signals
-    assert dut.q.value == 11
-
     # end
+
 
