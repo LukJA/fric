73e631a6e182d00caa8b30404d2fe5d3e785b5f3
Henry Franks <henryefranks@gmail.com>
2023-07-19 20:58:26 +0100

    Print setup paths for debugging

--- hdl/verif/cocotb/setup.sh
+++ hdl/verif/cocotb/setup.sh
@@ -1,5 +1,7 @@
 export COCOTB_TOP=$( cd $(dirname -- "$0") && pwd -P )
+echo COCOTB_TOP: $COCOTB_TOP
 export RTL_TOP=$( cd $COCOTB_TOP/../../rtl && pwd -P)
+echo RTL_TOP: $RTL_TOP
 
 export COCOTB_HDL_TIMEUNIT=1ns
 export COCOTB_HDL_TIMEPRECISION=1ps
