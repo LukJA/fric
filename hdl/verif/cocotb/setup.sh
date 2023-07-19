4375d546524f272167bd1817053dfec4d651a6e2
Henry Franks <henryefranks@gmail.com>
2023-07-19 21:08:44 +0100

    Fixes for github actions

--- hdl/verif/cocotb/setup.sh
+++ hdl/verif/cocotb/setup.sh
@@ -1,16 +1,13 @@
-echo $0
-echo $(dirname -- $0)
-echo $(pwd -P)
+# THIS FILE MUST BE RUN FROM THE CURRENT DIRECTORY
+# IT DOES NOT WORK OTHERWISE
 
-echo
-echo $(realpath $0)
-echo $( dirname "$(realpath $0)" )
-echo
+# IT IS CONFIGURED THIS WAY BECAUSE GITHUB ACTIONS IS A PAIN
+# (it runs all scripts from some weird temp directory)
 
-export COCOTB_TOP=$( cd $(dirname -- "$0") && pwd -P )
-echo COCOTB_TOP: $COCOTB_TOP
-export RTL_TOP=$( cd $COCOTB_TOP/../../rtl && pwd -P)
-echo RTL_TOP: $RTL_TOP
+# export COCOTB_TOP=$( cd $(dirname -- "$0") && pwd -P )
+export COCOTB_TOP=$( realpath . )
+export HDL_TOP=$( realpath $COCOTB_TOP/../.. )
+export RTL_TOP=$( realpath $HDL_TOP/rtl )
 
 export COCOTB_HDL_TIMEUNIT=1ns
 export COCOTB_HDL_TIMEPRECISION=1ps
