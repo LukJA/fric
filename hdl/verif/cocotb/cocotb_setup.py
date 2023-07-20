cbea228b72cf39173c922e6ccfe3d0070e7f8866
Henry Franks <henryefranks@gmail.com>
2023-07-20 13:28:10 +0100

    Dump waves for each test

--- hdl/verif/cocotb/cocotb_setup.py
+++ hdl/verif/cocotb/cocotb_setup.py
@@ -1,6 +1,7 @@
 import os, sys
 from cocotb.runner import get_runner
 
+COCOTB_TOP = os.getenv("COCOTB_TOP")
 RTL_TOP = os.getenv("RTL_TOP")
 
 # FIXME: these aren't currently included in the sim -- maybe buid_args?
@@ -16,15 +17,21 @@ if SIM is None:
     print("Error: SIM undefined!", file=sys.stderr)
     sys.exit(1)
 
+SIM_BUILD_ARGS=os.getenv("SIM_BUILD_ARGS", "").split(" ")
+
 
 def cocotb_test_module(src, toplevel, modulepath, modules):
     sys.path.append(modulepath)
+
+    subpath = modulepath.replace(f"{COCOTB_TOP}/tests/", "")
 
     runner = get_runner(SIM)
     runner.build(
         verilog_sources=src,
         hdl_toplevel=toplevel,
         always=True,
+        build_args=SIM_BUILD_ARGS,
+        build_dir=f"{COCOTB_TOP}/sim_build/{subpath}/toplevel"
     )
 
     runner.test(hdl_toplevel=toplevel, test_module=modules)
--- hdl/verif/cocotb/setup.sh
+++ hdl/verif/cocotb/setup.sh
@@ -22,5 +22,6 @@ export TOPLEVEL_LANG=verilog
 # export VERILOG_SOURCES=$(find ${RTL_TOP} -type f -name '*.sv')
 
 export SIM=verilator
+export SIM_BUILD_ARGS="--trace --coverage"
 
 export PYTHONPATH="${COCOTB_TOP}:${PYTHONPATH}"
