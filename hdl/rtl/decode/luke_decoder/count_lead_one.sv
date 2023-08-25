299f937e44b41a0b40ec31baecd748bd05633fdb
Luke Andrews <l.j.andrews.uk@gmail.com>
2023-08-25 23:02:59 +0100

    Fixes for CLO/CLZ recursive verilator implementations  move wrapper level out of parametric tree

--- hdl/rtl/decode/luke_decoder/count_lead_one.sv
+++ hdl/rtl/decode/luke_decoder/count_lead_one.sv
@@ -1,26 +1,15 @@
 // returns the signed integer value of the index of the first chosen bit
-import common::*;
 
-module count_lead_one #(
+module clo_internal #(
     parameter W_IN = 8,
-    parameter W_OUT = 8,
-    parameter __LEVEL = 1
+    parameter W_OUT = 8
 ) (
     input logic  [W_IN-1:0] a,
     output logic [W_OUT-1:0] q
 );
 
 generate
-    if (__LEVEL == 1) begin: wrapper // TOP EXPANDER
-
-        logic [$clog2(W_IN)-1:0] leading_ones;
-        count_lead_one #(.W_IN(W_IN), .W_OUT($clog2(W_IN)), .__LEVEL(0)) clo (.a(a), .q(leading_ones));
-
-        logic all_one;
-        assign all_one = &a;
-        assign q = (all_one) ? W_IN : {{W_OUT-$clog2(W_IN){1'b0}}, leading_ones};
-
-    end else if (W_IN == 2) begin: base // BASE CASE
+    if (W_IN == 2) begin: base // BASE CASE
         always_comb begin
         case (a[1])
             1'b1    :  q = 1'b1;
@@ -40,11 +29,32 @@ generate
         assign rhs =  a[(W_IN/2)-1:0];
         assign lhs_all_one = (&lhs);
 
-        count_lead_one #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1), .__LEVEL(0)) inner_block_r (.a(rhs), .q(half_count_rhs));
-        count_lead_one #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1), .__LEVEL(0)) inner_block_l (.a(lhs), .q(half_count_lhs));
+        clo_internal #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1)) inner_block_r (.a(rhs), .q(half_count_rhs));
+        clo_internal #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1)) inner_block_l (.a(lhs), .q(half_count_lhs));
         
         assign q = (lhs_all_one) ?  {1'b1, half_count_rhs} : {1'b0, half_count_lhs};
     end
 endgenerate
+
+endmodule
+
+
+module count_lead_one #(
+    parameter W_IN = 8,
+    parameter W_OUT = 8,
+    parameter __LEVEL = 1
+) (
+    input logic  [W_IN-1:0] a,
+    output logic [W_OUT-1:0] q
+);
+
+
+logic [$clog2(W_IN)-1:0] leading_ones;
+clo_internal #(.W_IN(W_IN), .W_OUT($clog2(W_IN))) clo (.a(a), .q(leading_ones));
+
+logic all_one;
+assign all_one = &a;
+assign q = (all_one) ? W_IN : {{W_OUT-$clog2(W_IN){1'b0}}, leading_ones};
+
 
 endmodule
--- hdl/rtl/decode/luke_decoder/count_lead_zero.sv
+++ hdl/rtl/decode/luke_decoder/count_lead_zero.sv
@@ -1,26 +1,15 @@
 // returns the signed integer value of the index of the first chosen bit
-import common::*;
 
-module count_lead_zero #(
+module clz_internal #(
     parameter W_IN = 8,
-    parameter W_OUT = 8,
-    parameter __LEVEL = 1
+    parameter W_OUT = 8
 ) (
     input logic  [W_IN-1:0] a,
     output logic [W_OUT-1:0] q
 );
 
 generate
-    if (__LEVEL == 1) begin: wrapper
-
-        logic [$clog2(W_IN)-1:0] leading_zeroes;
-        count_lead_zero #(.W_IN(W_IN), .W_OUT($clog2(W_IN)), .__LEVEL(0)) clo (.a(a), .q(leading_zeroes));
-
-        logic all_zero;
-        assign all_zero = ~(|a);
-        assign q = (all_zero) ? W_IN : {{W_OUT-$clog2(W_IN){1'b0}}, leading_zeroes};
-
-    end else if (W_IN == 2) begin: base
+    if (W_IN == 2) begin: base
         always_comb begin
         case (a[1])
             1'b0    :  q = 1'b1;
@@ -41,11 +30,30 @@ generate
         assign rhs =  a[(W_IN/2)-1:0];
         assign lhs_all_zero = ~(|lhs);
 
-        count_lead_zero #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1), .__LEVEL(0)) inner_block_r (.a(rhs), .q(half_count_rhs));
-        count_lead_zero #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1), .__LEVEL(0)) inner_block_l (.a(lhs), .q(half_count_lhs));
+        clz_internal #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1)) inner_block_r (.a(rhs), .q(half_count_rhs));
+        clz_internal #(.W_IN(W_IN/2), .W_OUT($clog2(W_IN)-1)) inner_block_l (.a(lhs), .q(half_count_lhs));
 
         assign q = (lhs_all_zero) ?  {1'b1, half_count_rhs} : {1'b0, half_count_lhs};
     end
 endgenerate
+
+endmodule
+
+
+module count_lead_zero #(
+    parameter W_IN = 8,
+    parameter W_OUT = 8,
+    parameter __LEVEL = 1
+) (
+    input logic  [W_IN-1:0] a,
+    output logic [W_OUT-1:0] q
+);
+
+logic [$clog2(W_IN)-1:0] leading_zeroes;
+clz_internal #(.W_IN(W_IN), .W_OUT($clog2(W_IN))) clo (.a(a), .q(leading_zeroes));
+
+logic all_zero;
+assign all_zero = ~(|a);
+assign q = (all_zero) ? W_IN : {{W_OUT-$clog2(W_IN){1'b0}}, leading_zeroes};
 
 endmodule
