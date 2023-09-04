331e9efeb015f31d1efde30e8877be4524c16069
Henry Franks <henryefranks@gmail.com>
2023-09-04 08:38:41 +0100

    Copy x2p function to new module backend

--- softmodels/src/pyposit/conversion/float.py
+++ softmodels/src/pyposit/conversion/float.py
@@ -1,49 +1,79 @@
 from math import isinf
+from ..operations.logic import twoc
 
-def float_to_posit(f: float, *, n: int = 32, es: int = 2) -> str:
-    n_bits = n
-    useed = pow(2, pow(2, es))
-
-    y = abs(f)
-    e = pow(2, es - 1)
-
+def float_to_posit(x: float, *, n: int = 32, es: int = 2) -> str:
+    """Generate posit string from float."""
     # quickly check these two unique cases first
-    if f == 0:
-        return '0' * n_bits
-    if isinf(f):
-        return '1' + '0' * (n_bits - 1)
+    if x == 0.0:
+        return '0' * n
+    if isinf(x):
+        return '1' + '0' * (n - 1)
+
+    ## grab sign and store
+    if x < 0:
+        sign = -1
+        x *= -1
+    else:
+        sign = 1
     
-    # if our number doesn't meet the special cases, solve iteratively
-    if f >= 0:  # northeast quadrant
-        p = 1
-        i = 2
+    useed = 2**(2**es)
+    ## first divide by useed or multiply by useed until it is in the 
+    ## interval [1, useed).
+    r = 1
+    if x < 1:
+        r = 0
+        while(x < 1):
+            x = x*useed
+            r = r - 1
+    elif x >= useed:
+        r = 1
+        while(x >= useed):
+            x = x/useed
+            r = r + 1
 
-        while y >= useed and i < n_bits:
-            p = 2*p + 1
-            y /= useed
-            i += 1
+    ## next divide by 2 or multiply by 2 until it is in the 
+    ## interval [1, 2).
+    e = 0
+    if x < 1:
+        while(x < 1):
+            x = x*2
+            e = e - 1
+    elif x >= 2:
+        while(x >= 2):
+            x = x/2
+            e = e + 1
 
-        p *= 2
-        i += 1
+    ## sign and regime
+    posstr = "0" #if sign == 1 else "1" ## wait till end for sign conversion
+    posstr += r*"1"+"0" if r > 0 else -1*r*"0"+"1"
 
-    else:  # southeast quadrant
-        p = 0
-        i = 1
+    ## exponent - bin string
+    e = format(e, 'b')
+    if len(e) < es:
+        e = "0"*(es - len(e)) + e
+    posstr += e
 
-        while y < 1 and i <= n_bits:
-            y *= useed
-            i += 1
-
-        if i >= n_bits:
-            p = 2
-        elif i == n_bits + 1:
-            p = 1
-        else:
-            i += 1
-
-    # extract exponent bits
-    while e > 0.5 and i <= n_bits:
-        p *= 2
+    if len(posstr) >= n:
+        posstr = posstr[:n]
+    else:
+        ## append fraction
+        ## iteratively encode the fraction (we can just keep going until we run out of precision)
+        ## TODO p.s. im not sure this actually follows the correct rounding spec, like the adder does
+        x -= 1
+        index = -1
+        while (len(posstr) < n):
+            if x >= 2**(index):
+                posstr += "1"
+                x -= 2**(index)
+            else:
+                posstr += "0"
+            index -= 1
+    
+    ## if we have a negative sign requested, take the two's complement form
+    if sign == -1:
+        posstr = twoc(posstr, len(posstr))
+    
+    return posstr
         
 def posit_to_float(p: str, *, n: int = 32, es: int = 2) -> float:
     """Generate float from posit string."""
