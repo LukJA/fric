972fef416e3edda0be0c425500a16c67fd44fb98
Henry Franks <henryefranks@gmail.com>
2023-09-17 14:44:47 +0100

    Allow environment path to be changed

--- setup.sh
+++ setup.sh
@@ -2,24 +2,33 @@
 # Welcome to the Python virtual environment setup script.
 # Luke this one's just for you :)
 
-echo On Debian based systems, python3-venv must be installed
-echo Run sudo apt install python3.10-venv
+# print warning if running on Linux
+if [ "$(uname -s)" = "Linux" ]; then
+    echo On Debian based systems, python3-venv must be installed
+    echo Run sudo apt install python3.10-venv
+fi
 
 # create environment in ./env
 # you can change this path if you want, so long as it's in .gitignore
-if [ ! -d "env" ]; then
-    echo "Creating virtual environment in ./env..."
-    python3 -m venv env
+envpath="env"
+
+# only create environment if it doesn't exist
+if [ -d "$envpath" ]; then
+    # actiavet only
+    echo "Found environment $envpath, activating..."
+    source "$envpath/bin/activate"
+else
+    echo "Creating virtual environment in $envpath..."
+    python3 -m venv "$envpath"
+
+    echo "Activating environment..."
+    source "$envpath/bin/activate"
+
+    # install requirements
+    echo "Installing Python requirements..."
+    python3 -m pip install -q --upgrade pip
+    python3 -m pip install -q -r requirements.txt
 fi
-
-# install requirements
-echo "Activating environment..."
-source env/bin/activate
-
-# install requirements
-echo "Installing python requirements..."
-python3 -m pip install -q --upgrade pip
-python3 -m pip install -q -r requirements.txt
 
 echo
 echo "Virtual environment setup complete!"
